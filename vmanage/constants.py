48f2237a04f40583d2984df9941d8a579913a664
kimdoanh89 <kimdoanh89@gmail.com>
2020-09-10 14:01:50 +0100

    update requirements.txt

--- vmanage/constants.py
+++ vmanage/constants.py
@@ -4,3 +4,11 @@ vmanage = {
     "username": "admin",
     "password": "admin"
 }
+
+## AlwaysOn SDWAN Sandbox
+# vmanage_sandbox = {
+#     "host": "sandbox-sdwan-1.cisco.com",
+#     "port": "443",
+#     "username": "devnetuser",
+#     "password": "RG!_Yw919_83"
+# }
